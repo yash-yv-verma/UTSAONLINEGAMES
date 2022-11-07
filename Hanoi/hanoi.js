386735241c722573161cdc2dfa1939f22c106e4f
Aidan Kollar <54418065+BandaidMan@users.noreply.github.com>
2022-11-07 19:22:34 +0000

    Added Hanoi page and created functions to display other game pages

---
+++ Hanoi/hanoi.js
@@ -0,0 +1,50 @@
+<hanoi>
+var MAX_SIZE = 12;
+var disks;
+
+var tower1 = [];
+var tower2 = [];
+var tower3 = [];
+
+var totalMoves = 0;
+
+tower1.push(12);
+  
+function resetGame() {
+  
+
+
+  totalMoves = 0;
+}
+
+function validMove() {
+
+  
+}
+  
+function moveDisk(from, to) {
+  if ( validMove(from, to) ) {
+
+
+    totalMoves += 1;    
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+</hanoi>
